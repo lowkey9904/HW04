ed7dd432daddacfa6412deebb168f4b8ad4d1e19
as468579 <as468579@gmail.com>
2019-12-02 16:06:03 +0800

    Initial Commit

---
+++ crudmovie/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  crudmovie
+//
+//  Created by User18 on 2019/12/2.
+//  Copyright © 2019 ntou. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
